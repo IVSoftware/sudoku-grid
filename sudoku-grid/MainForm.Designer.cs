837fd1b873b6daa857c9075a388b72fb6b39adc5
Tom Gregor <tgregor@ivsoftware.com>
2025-01-28 15:42:01 -0700

    Add project files.

---
+++ sudoku-grid/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace sudoku_grid
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gameBoard = new TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // gameBoard
+            // 
+            gameBoard.ColumnCount = 10;
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.ColumnStyles.Add(new ColumnStyle());
+            gameBoard.Location = new Point(27, 28);
+            gameBoard.Name = "gameBoard";
+            gameBoard.RowCount = 10;
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.RowStyles.Add(new RowStyle());
+            gameBoard.Size = new Size(452, 452);
+            gameBoard.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(510, 511);
+            Controls.Add(gameBoard);
+            Font = new Font("Segoe UI", 12F);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(4);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sudoku Grid";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel gameBoard;
+    }
+}
---
+++ sudoku-grid/MainForm.cs
@@ -0,0 +1,114 @@
+using System.Data.Common;
+using System.Drawing;
+using System.Security.Cryptography.Xml;
+
+namespace sudoku_grid
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int col = 0; col < 9; col++)
+            {
+                for (int row = 0; row < 9; row++)
+                {
+                    gameBoard.Controls.Add(new Square
+                    {
+                        Size = new Size(48, 48),
+                        Margin = new Padding(1),
+                        Anchor = (AnchorStyles)0xf,
+                        BackColor = Color.DarkGray,
+                        ForeColor = Color.WhiteSmoke,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Font = new Font(Font.FontFamily, 16),
+                    }, col, row);
+                }
+            }
+            InitGame();
+        }
+        Square GetSquare(int col, int row) =>
+            gameBoard
+            .GetControlFromPosition(col, row) as Square 
+            ?? throw new NullReferenceException();
+
+        void InitGame()
+        {
+            GetSquare(4, 0).FixedValue = 8;
+
+            GetSquare(1, 1).FixedValue = 2;
+            GetSquare(3, 1).FixedValue = 7;
+            GetSquare(4, 1).FixedValue = 1;
+            GetSquare(8, 1).FixedValue = 5;
+
+            GetSquare(0, 2).FixedValue = 8;
+            GetSquare(5, 2).FixedValue = 5;
+            GetSquare(8, 2).FixedValue = 1;
+
+            GetSquare(0, 3).FixedValue = 7;
+            GetSquare(2, 3).FixedValue = 9;
+            GetSquare(5, 3).FixedValue = 3;
+            GetSquare(6, 3).FixedValue = 4;
+
+            GetSquare(1, 4).FixedValue = 4;
+            GetSquare(3, 4).FixedValue = 5;
+            GetSquare(8, 4).FixedValue = 3;
+
+            GetSquare(2, 5).FixedValue = 5;
+            GetSquare(7, 5).FixedValue = 7;
+            GetSquare(8, 5).FixedValue = 9;
+
+            GetSquare(0, 6).FixedValue = 5;
+            GetSquare(4, 6).FixedValue = 7;
+            GetSquare(7, 6).FixedValue = 9;
+            GetSquare(8, 6).FixedValue = 2;
+
+            GetSquare(2, 7).FixedValue = 6;
+            GetSquare(3, 7).FixedValue = 3;
+
+            GetSquare(1, 8).FixedValue = 9;
+            GetSquare(2, 8).FixedValue = 2;
+            GetSquare(3, 8).FixedValue = 4;
+            GetSquare(6, 8).FixedValue = 7;
+        }
+
+
+        class Square : Label
+        {
+            public new string Text
+            {
+                get => base.Text;
+                set { }
+            }
+            public int? Value
+            {
+                get => _value;
+                set
+                {
+                    if (!Equals(_value, value))
+                    {
+                        _value = value;
+                        base.Text = $"{value}";
+                    }
+                }
+            }
+            int? _value = default;
+
+            public bool IsFixedValue { get; private set; }
+
+            public int? FixedValue
+            {
+                set
+                {
+                    Value = value;
+                    IsFixedValue = value is not null;
+                    BackColor = 
+                        IsFixedValue
+                        ? Color.FromArgb(0x22, 0x22,0x22)
+                        : Color.DarkGray;
+                }
+            }
+        }
+    }
+}
